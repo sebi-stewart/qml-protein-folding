5c42b331a2ee3713b52a81ba8edf50947d038f21
Sebi <sebastianstewart03@gmail.com>
2026-03-25 12:24:22 +0000

    Adding code so that running iteratiosn of the test is easier

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import contextlib
+import time
+
+import pandas as pd
 import pyrosetta
 
 from benchmark import full_bpti_benchmark
-from energy_calculation import calculate_and_compare_energies
 from misc import init_basic_params, default_qaoa_params, BasicParams, QAOAParams
 from rotamer_extraction import extract_top_n_rotamers
-from h_ising_creation import build_ising_hamiltonian, extract_and_reduce_tensors
+from h_ising_creation import extract_and_reduce_tensors
 from initialisation import initialize_rosetta
-from custom_qaoa import qaoa_func_generator, run_qaoa
-from h_mixer import custom_xy_mixer_layer
+from run import run, RunConfig, extract_rank_matches
+import sys
 
-from validation import validate_conformations
+old_stdout = sys.stdout
+
+log_dir = "logs"
+
 
 if __name__ == '__main__':
     initialize_rosetta(pyrosetta, extra_flags="-mute all")
@@ -21,21 +27,56 @@ if __name__ == '__main__':
     # Generating QUBO (Quadratic Unconstrained Binary Optimisation) Model, and then reduce it
     h_linear, J_quadratic, global_offset = extract_and_reduce_tensors(residue_library, ig)
     basic_params: BasicParams = init_basic_params(h_linear)
-    qaoa_params: QAOAParams = default_qaoa_params()
+    base_qaoa_params: QAOAParams = default_qaoa_params()
 
-    # Generate the actual observable and running functions we will use in the QAOA Algorithm
-    H_ising = build_ising_hamiltonian(h_linear, J_quadratic)
-    cost_function, sample_function = qaoa_func_generator(H_ising, custom_xy_mixer_layer, basic_params)
+    p_runs = [1, 2, 3, 4, 8, 12, 16, 32]
+    seed_versions = [1, 2, 3, 5, 7, 42]
 
-    # Run the Quantum Approximate Optimisation Algorithm and sample the final parameters
-    final_params = run_qaoa(cost_function, qaoa_params)
-    probabilities = sample_function(final_params)
+    run_configs = [
+        RunConfig(
+            QAOAParams(p, base_qaoa_params.seed,
+                       base_qaoa_params.optimiser_stepsize,
+                       base_qaoa_params.epochs)
+            , f"run_layers={p}_s.log") for p in p_runs
+    ]
 
-    # Extract the top 100 most probably conformations and check that exactly 1 rotamer for each residue is selected
-    valid_conformations = validate_conformations(probabilities, basic_params)
+    results: dict[int, list[int]] = {}
+    run_records = []
 
-    # Calculate both the quantum and pyrosetta energies for comparison
-    calculate_and_compare_energies(valid_conformations,
-                                   h_linear, J_quadratic, global_offset,
-                                   benchmark_pose, scorefxn, residue_library,
-                                   basic_params)
+    for config in run_configs:
+        layers = config.qaoaParams.layers
+        print(f"===== Starting run (Layers: {layers}) - {len(seed_versions)} seeds =====")
+
+        start = time.perf_counter()
+
+        with open(f"{log_dir}/{config.log_file}", "w") as log_file:
+            with contextlib.redirect_stdout(log_file):
+                for seed in seed_versions:
+                    config.qaoaParams.seed = seed
+
+                    # Execute the quantum pipeline
+                    valid_conformations = run(
+                        h_linear, J_quadratic, global_offset,
+                        benchmark_pose, scorefxn, residue_library,
+                        basic_params, config.qaoaParams
+                    )
+
+                    # Extract metrics
+                    prob_rank_match = extract_rank_matches(valid_conformations)
+
+                    # SAVE RAW DATA: Append every single run as an independent record
+                    run_records.append({
+                        'layers': layers,
+                        'seed': seed,
+                        'rank_matches': prob_rank_match
+                    })
+
+        end = time.perf_counter()
+        time_taken = end - start
+        print(f"===== Run Complete time taken = {time_taken:5.3f} seconds | {time_taken / len(seed_versions):5.3f} per run =====\n")
+
+
+    print("\n\n=============== All Runs Complete Saving to DF ===============\n\n")
+
+    df = pd.DataFrame(run_records)
+    df.to_pickle("results/run2_raw.pkl")
---
+++ run.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+
+import numpy as np
+from pyrosetta import Pose
+
+from energy_calculation import calculate_and_compare_energies
+from misc import BasicParams, QAOAParams
+from rotamer_extraction import TrackedResidue
+from h_ising_creation import build_ising_hamiltonian
+from custom_qaoa import qaoa_func_generator, run_qaoa
+from h_mixer import custom_xy_mixer_layer
+
+from validation import validate_conformations, Conformation
+
+@dataclass
+class RunConfig:
+    qaoaParams: QAOAParams
+    log_file: str
+
+def run(h_linear: dict[int, dict[int, float]], J_quadratic: dict, global_offset: float,
+        benchmark_pose: Pose, scorefxn: object, residue_library: dict[int, TrackedResidue],
+        basic_params: BasicParams, qaoa_params: QAOAParams) -> list[Conformation]:
+    # Generate the actual observable and running functions we will use in the QAOA Algorithm
+    H_ising = build_ising_hamiltonian(h_linear, J_quadratic)
+    cost_function, sample_function = qaoa_func_generator(H_ising, custom_xy_mixer_layer, basic_params)
+
+    # Run the Quantum Approximate Optimisation Algorithm and sample the final parameters
+    final_params = run_qaoa(cost_function, qaoa_params)
+    probabilities = sample_function(final_params)
+
+    # Extract the top 100 most probably conformations and check that exactly 1 rotamer for each residue is selected
+    valid_conformations = validate_conformations(probabilities, basic_params)
+
+    # Calculate both the quantum and pyrosetta energies for comparison
+    calculate_and_compare_energies(valid_conformations,
+                                   h_linear, J_quadratic, global_offset,
+                                   benchmark_pose, scorefxn, residue_library,
+                                   basic_params)
+
+    return valid_conformations
+
+def extract_rank_matches(valid_conformations: list[Conformation]):
+    energies: list[dict[str, np.float64 | int | None]] = [
+        {"quantum_energy": conf.quantum_energy, "probability": conf.probability} for conf in valid_conformations
+    ]
+
+    energies.sort(key=lambda conf: conf['probability'], reverse=True)
+    for i, conf in enumerate(energies):
+        conf['probs_idx'] = i
+
+    energies.sort(key=lambda conf: conf['quantum_energy'])
+    for i, conf in enumerate(energies):
+        conf['quant_idx'] = i
+
+    return [abs(conf['probs_idx'] - conf['quant_idx']) for idx, conf in enumerate(energies)]

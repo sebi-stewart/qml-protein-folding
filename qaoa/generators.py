be5247ec08e2cf807e8c216088b46845c911b817
Sebi <sebastianstewart03@gmail.com>
2026-04-05 19:54:24 +0100

    Updating the ansatz for the the qaoa

--- qaoa/generators.py
+++ qaoa/generators.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pennylane as qml
 from qaoa.objects import BasicParams
 
@@ -12,17 +13,26 @@ def qaoa_func_generator(dev, H_ising, mixer_layer, generator_params: BasicParams
         qml.qaoa.cost_layer(gamma, H_ising)
         mixer_layer(beta, wire_offsets, seq_positions, rotamer_counts)
 
+    def qaoa_init_layer():
+        for seq in seq_positions:
+            k = rotamer_counts[seq]
+
+            state_vector = np.zeros(2**k)
+            for i in range(k):
+                one_hot_int = 2 ** (k - 1 - i)
+                state_vector[one_hot_int] = 1.0 / np.sqrt(k)
+
+            base_wire = wire_offsets[seq]
+            bundle_wires = list(range(base_wire, base_wire + k))
+
+            qml.StatePrep(state_vector, wires=bundle_wires)
+
     @qml.qnode(dev, interface="jax", diff_method="adjoint")
     def cost_function(params):
         gammas = params[0]
         betas = params[1]
 
-        # 1. Custom initialisation
-        for seq in seq_positions:
-            base_wire = wire_offsets[seq]
-            qml.PauliX(wires=base_wire) # First rotamer set to 1
-
-        # 2. Apply p layers of QAOA
+        qaoa_init_layer()
         for i in range(len(gammas)):
             qaoa_layer(gammas[i], betas[i])
 
@@ -34,10 +44,7 @@ def qaoa_func_generator(dev, H_ising, mixer_layer, generator_params: BasicParams
         gammas = params[0]
         betas = params[1]
 
-        for seq in seq_positions:
-            base_wire = wire_offsets[seq]
-            qml.PauliX(wires=base_wire)
-
+        qaoa_init_layer()
         for i in range(len(gammas)):
             qaoa_layer(gammas[i], betas[i])
 

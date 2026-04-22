55409f872e92e07882b31886fae0f5587ef7558c
Sebi <sebastianstewart03@gmail.com>
2026-04-22 22:42:46 +0100

    Evaluating the final results and improevement of the classifications system

--- phase2/main.py
+++ phase2/main.py
@@ -22,6 +22,8 @@ import pandas as pd
 
 from qaoa.scoring import _get_valid_bitstrings_matrix
 
+from collections import Counter
+
 
 def extract_one_body_energies_from_instance(inst: ExtractionTestInstance, logger: logging.Logger):
     pose, residue_library, ig, rot_sets, scorefxn = run_pyrosetta_obj_extraction(
@@ -35,7 +37,7 @@ def extract_one_body_energies_from_instance(inst: ExtractionTestInstance, logger
     return one_body, two_body, pose, residue_library, ig, rot_sets, scorefxn
 
 qubit_to_shot_map = {
-    5: 10, 6: 10, 7: 100, 8: 10, 9: 100, 10: 100, 11: 100, 12: 100, 13: 100, 14: 100, 18: 100, 22: 1000
+    5: 10, 6: 10, 7: 100, 8: 10, 9: 10, 10: 100, 11: 100, 12: 100, 13: 100, 14: 100, 18: 100, 22: 400
 }
 
 def get_sample_function_for_phase_2(logger: logging.Logger, one_body, two_body):
@@ -56,7 +58,6 @@ def get_sample_function_for_phase_2(logger: logging.Logger, one_body, two_body):
 def extract_best_qaoa_params(qaoa_file_path: str):
     assert qaoa_file_path.endswith(".npz"), "Expected a .npz file containing the QAOA results"
     data = np.load(qaoa_file_path, allow_pickle=True)
-    print(data)
 
     # Act as an oracle to extract the relevant data for the next phase
     # We refrain from looking at the actual values, but we know the structure of the saved data from the layered_run function
@@ -115,7 +116,6 @@ def evaluate_singular_pyrosetta_energy(bitstring: list[int], pose,
 
     #Flexible rotamers
     for seq in seq_positions:
-        # print(f"Evaluating seq {seq} - bitstring controls this position, checking rotamer bits {wire_offsets[seq]} to {wire_offsets[seq] + rotamer_counts[seq] - 1}")
         base_wire = wire_offsets[seq]
         num_rots = rotamer_counts[seq]
 
@@ -132,8 +132,6 @@ def evaluate_singular_pyrosetta_energy(bitstring: list[int], pose,
     for seq in all_seq:
         if seq in seq_positions: continue
 
-        # print(f"Setting fixed rotamer for seq {seq} - bitstring does not control this position")
-
         res_entry = residue_library[seq]
         rotamer_entry = res_entry.rotamers[0]
 
@@ -141,11 +139,10 @@ def evaluate_singular_pyrosetta_energy(bitstring: list[int], pose,
 
     return new_pose
 
-def exhaustively_evaluate_all_conformations(unique_bitstrings: list[list[int]], original_pose, scorefxn, residue_library: dict[int, TrackedResidue], params: BasicParams):
+def exhaustively_evaluate_all_conformations(unique_bitstrings, original_pose, scorefxn, residue_library: dict[int, TrackedResidue], params: BasicParams):
     conformations = []
     for nd_bitstring in unique_bitstrings:
         bitstring = list(map(int, nd_bitstring))
-        # print(list(map(int, bitstring)))
         new_pose = evaluate_singular_pyrosetta_energy(bitstring, original_pose, residue_library, params)
         biological_energy = np.float64(scorefxn(new_pose))
         conformations.append(
@@ -181,7 +178,7 @@ def extract_best_conformation_for_seeds(processed_results: dict[int, set[tuple[i
 
     return best_conformations_per_seed
 
-def main(logger: logging.Logger, fac: TestInstanceFactory, results_file: str = "0/phase2_5_to_10_qubits/AF-5PTI_52_55_4_12_layers.npz"):
+def main(logger: logging.Logger, fac: TestInstanceFactory, results_file: str = "0/phase2_5_to_10_qubits/AF-5PTI_52_55_4_12_layers.npz", exhaustive_evaluation=False):
     stripped_file_name = results_file.split("/")[-1].split(".")[0]
     test_name, start_str, end_str, rot_count_str, _, _ = stripped_file_name.split("_")
     start, end, rot_count = int(start_str), int(end_str), int(rot_count_str)
@@ -196,17 +193,18 @@ def main(logger: logging.Logger, fac: TestInstanceFactory, results_file: str = "
 
     logger.info(f"Extracting one-body and two-body energies for instance {inst.test_name}...")
     phase2_rescoring_logger = logging.getLogger("qaoa.rescoring_phase2")
-    one_body, two_body, pose, residue_library, ig, rot_sets, scorefxn = extract_one_body_energies_from_instance(inst, phase2_rescoring_logger)
-    logger.info(f"Extracted residues: {residue_library.keys()} --- {residue_library}")
-    logger.info(f"Extracted one-body energies: {one_body.keys()} --- {one_body}")
+    hidden_logger = logging.getLogger("qaoa.hidden")
+    one_body, two_body, pose, residue_library, ig, rot_sets, scorefxn = extract_one_body_energies_from_instance(inst, hidden_logger)
+    hidden_logger.info(f"Extracted residues: {residue_library.keys()} --- {residue_library}")
+    hidden_logger.info(f"Extracted one-body energies: {one_body.keys()} --- {one_body}")
 
-    sample_func, basic_params = get_sample_function_for_phase_2(phase2_rescoring_logger, one_body, two_body)
+    sample_func, basic_params = get_sample_function_for_phase_2(hidden_logger, one_body, two_body)
 
     logger.info("Extracting best QAOA parameters from file...")
     best_params = extract_best_qaoa_params(results_file)
     processed_results, unique_bitstrings = get_and_process_shot_results(sample_func, best_params, phase2_rescoring_logger)
 
-    logger.info(f"Evaluating PyRosetta energies for unique bitstrings... sequences: {basic_params.seq_positions}, wire offsets: {basic_params.wire_offsets}, rotamer counts: {basic_params.rotamer_counts}")
+    logger.info(f"Evaluating PyRosetta energies for unique bitstrings... Total unique conformations to evaluate: {len(unique_bitstrings)}")
     scored_conformations = evaluate_pyrosetta_energies(unique_bitstrings, pose, scorefxn, residue_library, basic_params)
     base_conformation = RescoringConformation(bitstring=None, pose=pose, biological_energy=np.float64(scorefxn(pose)))
     compare_scoring_results(scored_conformations, base_conformation, logger)
@@ -217,7 +215,7 @@ def main(logger: logging.Logger, fac: TestInstanceFactory, results_file: str = "
     results = []
     win_count, tie_count, loss_count = 0, 0, 0
     for seed, conf in best_conf_per_seed.items():
-        results.append({
+        cur_result = {
             'seed': seed,
             'bitstring': conf.bitstring,
             'biological_energy': conf.biological_energy,
@@ -226,23 +224,23 @@ def main(logger: logging.Logger, fac: TestInstanceFactory, results_file: str = "
             'residues': f"{inst.residue_start}_{inst.residue_end}",
             'residue_count': inst.residue_end - inst.residue_start + 1,
             'rotamers': inst.rotamer_count,
-        })
+            'num_qubits': basic_params.num_qubits,
+        }
 
-        logger.debug(
-            f"Seed {seed}: Best Bitstring: {conf.bitstring}, Biological Energy: {conf.biological_energy:.4f}, Energy Difference: {conf.energy_diff:.4f}")
-        if abs(conf.energy_diff) < epsilon_value:
-            logger.debug(
-                f"Seed {seed} is too close to the original pose (Energy difference {conf.energy_diff:.4f} < {epsilon_value}), it's a tie.")
-            tie_count += 1
+        if abs(conf.energy_diff) <= epsilon_value:
+            cur_result["classification"] = 'tie'
         elif conf.energy_diff < 0:
-            logger.debug(
-                f"Seed {seed} has a better conformation than the original pose (Energy difference {conf.energy_diff:.4f} < 0), it's a success!")
-            win_count += 1
+            cur_result["classification"] = 'win'
         else:
-            logger.debug(
-                f"Seed {seed} has a worse conformation than the original pose (Energy difference {conf.energy_diff:.4f} > 0), it's a failure.")
-            loss_count += 1
-    logger.info(f"Summary of results: {win_count} wins, {tie_count} ties, {loss_count} losses out of {len(best_conf_per_seed)} seeds.")
+            cur_result["classification"] = 'loss'
+        results.append(cur_result)
+
+    classification_counts = Counter(result['classification'] for result in results)
+    logger.info(f"Summary of results: {classification_counts} losses out of {len(best_conf_per_seed)} seeds.")
+
+    if not exhaustive_evaluation:
+        logger.debug("Skipping exhaustive evaluation of all conformations. To enable this, set exhaustive_evaluation=True when calling main().")
+        return results
 
     X_matrix, indices = _get_valid_bitstrings_matrix(basic_params, logger)
     logger.info("Exhaustively evaluating all conformations from all possible bitstrings (not just the best per seed) to get a more complete picture of the energy landscape...")
@@ -262,25 +260,41 @@ def main(logger: logging.Logger, fac: TestInstanceFactory, results_file: str = "
             break
     logger.info("Best conformation from the exhaustive search:")
     best_exhaustive_conf = min(exhaustive_conf, key=lambda conf: conf.energy_diff)
+    worst_exhaustive_conf = max(exhaustive_conf, key=lambda conf: conf.energy_diff)
     logger.info(
         f"Bitstring: {best_exhaustive_conf.bitstring}, Biological Energy: {best_exhaustive_conf.biological_energy:.4f}, Energy Difference: {best_exhaustive_conf.energy_diff:.4f}")
-    return results
+    logger.info(
+        f"Worst Conformation - Bitstring: {worst_exhaustive_conf.bitstring}, Biological Energy: {worst_exhaustive_conf.biological_energy:.4f}, Energy Difference: {worst_exhaustive_conf.energy_diff:.4f}. Dumped pose to worst_conformation.pdb for further analysis."
+    )
+    return results, worst_exhaustive_conf
 
 if __name__ == "__main__":
-    print(list(pathlib.Path("").rglob("*_12_layers.npz")))
     logger = setup_logging("rescoring_phase2", "5PTI")
     initialize_rosetta(pyrosetta, extra_flags="-mute all")
     fac = TestInstanceFactory()
 
     results = []
-    for file in pathlib.Path("").rglob("*_12_layers.npz"):
+    worst_confs = []
+    for file in pathlib.Path("Oldruns_without_extra_rotamers").rglob("*_12_layers.npz"):
+        # if "oldruns" in str(file).lower(): continue
+        # if "5PTI_57_60_4_12_layers" not in str(file): continue  # Temporary filter to focus on a specific instance, remove this to run on all files
+
         logger.info(f"Processing file: {str(file)}")
-        cur_results = main(logger, fac, str(file))
+        cur_results, worst_conf = main(logger, fac, str(file), exhaustive_evaluation=True)
+        worst_confs.append(worst_conf)
 
         results.extend(cur_results)
 
         logger.info(f"Completed processing for {str(file)}. Current aggregated results count: {len(results)}\n\n\n")
-        break
 
-    pd.DataFrame(results).to_pickle("phase2_results.pkl")
+    worst_exhaustive_conf = max(worst_confs, key=lambda conf: conf.energy_diff)
+    logger.info("Overall Worst Conformation Across All Instances:")
+    logger.info(
+        f"Bitstring: {worst_exhaustive_conf.bitstring}, Biological Energy: {worst_exhaustive_conf.biological_energy:.4f}, Energy Difference: {worst_exhaustive_conf.energy_diff:.4f}. Dumped pose to worst_conformation.pdb for further analysis."
+    )
+    worst_pose = worst_exhaustive_conf.pose
+    worst_pose.dump_pdb("worst_conformation.pdb")
+
+
+    pd.DataFrame(results).to_pickle("phase2_old_runs2.pkl")
 

a7510139880965d2765ffbc610ad6c89b40d621b
Sebi <sebastianstewart03@gmail.com>
2026-06-27 22:44:46 +0100

    Moving files around, and creating dedicated files for the plots used in the publication

---
+++ utils/make_paths_absolute.py
@@ -0,0 +1,15 @@
+from os import chdir
+from pathlib import Path
+
+
+def make_paths_relative_to_root():
+    """Always use the same, absolute (relative to root) paths
+
+    which makes moving the notebooks around easier.
+    """
+    top_level = Path(__file__).parent.parent
+
+    chdir(top_level)
+
+
+make_paths_relative_to_root()
